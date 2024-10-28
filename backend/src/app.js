00d9953045720260fb6c74125f29dbec7f4f3b3e
VenusCh001 <venus222btcse22@igdtuw.ac.in>
2024-10-28 13:36:24 +0530

    initialised repo

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express=require("express")
 const app=express();
-const port=process.env.PORT || 
-
+const port=process.env.PORT || 3000
+require("./db/conn")
 app.get("/",(req,res)=>{
     res.send("hello i am venus")
 });
 
-app.listen()
+app.listen(port,()=>{
+    console.log('server is running at ${port}');
+})
