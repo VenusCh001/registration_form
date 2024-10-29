9d9698e2091b0bae0f47b89a58cb00b829ec8b78
VenusCh001 <venus222btcse22@igdtuw.ac.in>
2024-10-29 15:58:46 +0530

    rectify

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,22 @@
 const express=require("express")
-const path=require("path");
 const app=express();
-const port=process.env.PORT || 3000
+const path=require("path");
 const hbs=require("hbs");
+
+require("./db/conn")
+
+const port=process.env.PORT || 3000
+
 
 const static_path=path.join(__dirname,"../public");
 const template_path=path.join(__dirname,"../templates/views");
 const partials_path=path.join(__dirname,"../templates/partials");
 
-require("./db/conn")
 
 app.use(express.static(static_path));
 app.set("view engine","hbs");
 app.set("views",template_path);
-hbs.registerPartials(partials_path)
+hbs.registerPartials(partials_path);
 
 app.get("/",(req,res)=>{
     res.render("index")
