f96707c1dc76efa42e78667f2049a0c0e2091fa4
VenusCh001 <venus222btcse22@igdtuw.ac.in>
2024-10-29 15:51:10 +0530

    connected hbs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,22 @@
 const express=require("express")
+const path=require("path");
 const app=express();
 const port=process.env.PORT || 3000
+const hbs=require("hbs");
+
+const static_path=path.join(__dirname,"../public");
+const template_path=path.join(__dirname,"../templates/views");
+const partials_path=path.join(__dirname,"../templates/partials");
+
 require("./db/conn")
+
+app.use(express.static(static_path));
+app.set("view engine","hbs");
+app.set("views",template_path);
+hbs.registerPartials(partials_path)
+
 app.get("/",(req,res)=>{
-    res.send("hello i am venus")
+    res.render("index")
 });
 
 app.listen(port,()=>{
--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,1 +1,6 @@
-
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/registration_form_db").then(() => {
+    console.log("connection successfull !!!");
+}).catch((e) => {
+    console.log("no connection ");
+})
